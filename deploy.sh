e723e5c89a70a929b05496448a5c07d816db6a22
Antonio P Lacerda <antonioplacerda@gmail.com>
2020-02-29 08:50:52 +0000

    also commit the main content dir

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/zsh
 
 # If a command fails then the deploy stops
-set -e
+# set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
@@ -22,4 +22,11 @@ fi
 git commit -m "$msg"
 
 # Push source and build repos.
+git push origin master
+
+printf "\ncommiting content\n"
+
+cd ../content
+git add .
+git commit -m "$msg"
 git push origin master
